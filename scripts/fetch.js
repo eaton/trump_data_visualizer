f40d6d68cc8109af25b264dc2054bc81acc79322
Jeff Eaton <jeff@eaton.fyi>
2025-04-12 23:18:37 -0500

    Don't fetch the data that already uses dynamic loaders

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -5,14 +5,6 @@ const baseUrl = 'https://raw.githubusercontent.com/harrisj/trump_data/refs/heads
 const outputBase = './src/data/';
 
 const files = [
-  'it_modernization/raw_data/agencies.yaml',
-  'it_modernization/raw_data/aliases.yaml',
-  'it_modernization/raw_data/cases.yaml',
-  'it_modernization/raw_data/details.yaml',
-  'it_modernization/raw_data/events.yaml',
-  'it_modernization/raw_data/onboard_docs.yaml',
-  'it_modernization/raw_data/roundups.yaml',
-  'it_modernization/raw_data/systems.yaml',
   'gitmo_camp/gitmo_camp.csv',
   'home_visits/home_visits.csv',
   'home_visits/golf_dates.csv',
